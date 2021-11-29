852f854262eda43d90d3173a5c34492328a0a311
Lock1 <30568743+Lock1@users.noreply.github.com>
2021-11-29 19:14:39 +0700

    Perbaikan penulisan simple lock
    Perbaikan output pada simple lock dan refaktor kode

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # concurrency-control-protocol
 
-# MVCC Implementation
-Untuk menjalan program mvcc.py diperlukan :
-- python3
-- command untuk run program : python3 mvcc.py
+# Simple Locking
+Lakukan kompilasi menggunakan compiler g++ dan library thread
+`g++ -pthread -o out simple_lock.cpp` dan jalankan hasil output
+
+# OCC & MVCC Implementation
+Untuk menjalan program occ.py dan mvcc.py diperlukan :
+- `python3`
+- command untuk run program : `python3 <program>`
 
 config pada mvcc.py sudah dijelaskan pada program, jika ingin mengedit config ikuti arahan komentar pada program
--- src/simple_lock.cpp
+++ src/simple_lock.cpp
@@ -2,17 +2,23 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <string>
 
 class Resource {
 private:
     int data;
+    std::string name;
 public:
-    Resource() {
-
+    Resource(std::string n) {
+        name = n;
     }
 
     int getData() {
         return data;
+    }
+
+    std::string getName() {
+        return name;
     }
 
     void setData(int x) {
@@ -31,10 +37,6 @@ private:
 
 public:
     SimpleLockManager() {
-
-    }
-
-    ~SimpleLockManager() {
     }
 
     void lock(Resource& target) {
@@ -42,7 +44,7 @@ public:
         // Using logical and short-circuit
         for (unsigned i = 0; i < locked_res.size(); ++i)
             while (i < locked_res.size() && (*locked_res[i]) == target)
-                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         locked_res.push_back(&target);
     }
@@ -58,41 +60,71 @@ public:
 };
 
 
+class Transaction {
+private:
+    SimpleLockManager &lock_manager_ref;
+    std::string transaction_name;
+    std::vector<Resource*> locked_res;
+public:
+    Transaction(std::string n, SimpleLockManager &lm) : lock_manager_ref(lm) {
+        transaction_name = n;
+        std::cout << "Transaction " << transaction_name << " started\n";
+    }
+
+    void write(Resource &res, int value) {
+        std::cout << "W" << transaction_name << "(" << res.getName() << ")\n";
+        res.setData(value);
+    }
+
+    int read(Resource &res) {
+        std::cout << "R" << transaction_name << "(" << res.getName() << ")\n";
+        return res.getData();
+    }
+
+    void exclusiveLock(Resource &res) {
+        lock_manager_ref.lock(res);
+        locked_res.push_back(&res);
+        std::cout << "XL" << transaction_name << "(" << res.getName() << ") acquired\n";
+    }
+
+    void unlock(Resource &res) {
+        lock_manager_ref.unlock(res);
+        std::cout << "U" << transaction_name << "(" << res.getName() << ") released\n";
+    }
+
+    void commit() {
+        for (auto itr = locked_res.begin(); itr != locked_res.end(); ++itr)
+            lock_manager_ref.unlock(**itr);
+        std::cout << "Transaction " << transaction_name << " committed\n";
+    }
+};
 
 
 
-Resource a, b;
+
+
 SimpleLockManager manager;
-void transaction1() {
-    manager.lock(a);
-    std::cout << "XL1(A) acquired\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << "W(A) 10000\n";
-    a.setData(10000);
-    manager.unlock(a);
-    std::cout << "XL1(A) released\n";
+Resource a = Resource("A");
+Resource b = Resource("B");
 
-    manager.lock(b);
-    std::cout << "XL1(B) acquired\n";
-    std::cout << "W(B) " << b.getData() << " + 800000\n";
-    b.setData(b.getData() + 800000);
-    manager.unlock(b);
-    std::cout << "XL1(B) released\n";
+void transaction1() {
+    Transaction t1 = Transaction("1", manager);
+    t1.exclusiveLock(a);
+    t1.write(a, 10000);
+    t1.unlock(a);
+
+    t1.exclusiveLock(b);
+    t1.write(b, t1.read(b) + 800000);
+    t1.commit();
 }
 
 void transaction2() {
-    manager.lock(a);
-    manager.lock(b);
-    std::cout << "XL2(A) acquired\n";
-    std::cout << "XL2(B) acquired\n";
-    std::cout << "W(A) " << a.getData() << " + 800000\n";
-    a.setData(a.getData() + 40);
-    std::cout << "W(B) 900\n";
-    b.setData(900);
-    manager.unlock(b);
-    manager.unlock(a);
-    std::cout << "XL2(A) released\n";
-    std::cout << "XL2(B) released\n";
+    Transaction t2 = Transaction("2", manager);
+    t2.exclusiveLock(a);
+    t2.exclusiveLock(b);
+    t2.write(a, t2.read(a) + 40);
+    t2.write(b, 900);
+    t2.commit();
 }
 
 int main() {
@@ -103,7 +135,7 @@ int main() {
     thtr1.join();
     thtr2.join();
 
-    std::cout << "Result\n";
+    std::cout << "\nResult\n";
     std::cout << a.getData() << "\n";
     std::cout << b.getData() << "\n";
     return 0;
